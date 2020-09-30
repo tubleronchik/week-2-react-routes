04f97f74851e7c3cabfc571e24a85fbde4318ac9
tubleronchik <m.bystramovich@gmail.com>
2020-09-30 16:24:37 +0300

    update profile

--- client/components/home.js
+++ client/components/home.js
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Switch>
-            <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
             <Route exact path="/dashboard/" component={() => <Dash />} />
             <Route exact path="/dashboard/main" component={() => <Main />} />
           </Switch>
