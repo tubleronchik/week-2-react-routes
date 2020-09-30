e41a4cfab0493cc9c498270931274d62a65c9da9
tubleronchik <m.bystramovich@gmail.com>
2020-09-30 13:06:41 +0300

    added switch

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Header from './header'
 import Profile from './profile'
@@ -12,9 +12,12 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
-          <Route exact path="/dashboard/" component={() => <Dash />} />
-          <Route exact path="/dashboard/main" component={() => <Main />} />
+          <Switch>
+            <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
+            <Route exact path="/dashboard/" component={() => <Dash />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+          </Switch>
+          
         </div>
       </div>
     </div>
